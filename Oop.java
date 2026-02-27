753bfe7e062910e23fd083b5fd35695a3b6f61a4
Pratyush Dev <fate17x@gmail.com>
2026-02-27 18:08:56 +0530

    this

--- Oop.java
+++ Oop.java
@@ -1,18 +1,30 @@
 public class Oop{
+    int x;
+    String fname, lname;
+
+    public Oop(){
     int x=0;
-    String fname = "John";
-    String lname = "Doe";
-    static void myMethod(String x) {
-        System.out.println(x);
+    fname = "John";
+    lname = "Doe";
+    }
+    
+    static void myMethod(String x) { //no need to create an object to call it
+    System.out.println(x);
+    }
+
+    public Oop(int x, String fname, String lname){
+        this.x=y;
+        this.fname=fname;
+        this.lname=lname;
     }
     public static void main(String[] args){
-        Oop nclass= new Oop();
+        Oop nclass= new Oop(5,"lol ","dev");
         Oop nc2= new Oop();
         nc2.x=14;
+        System.out.println(nclass.x);
         myMethod("blolob");
         System.out.println(nclass.fname+nclass.lname);
         nc2.myMethod("lmao");
-        System.out.println(nc2.x);
-
+        System.out.println(nc2.x+nc2.fname+nc2.lname);
     }
 }
